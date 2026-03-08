f864d606bd05aeaf94dcf1b01327f1ee12bafb74
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:47 -0500

    fix: mark header-dependent routes as dynamic

--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import MeetingCard from '../components/MeetingCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Meetings | Town',
--- app/officials/page.tsx
+++ app/officials/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import OfficialCard from '../components/OfficialCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Officials | Town',
